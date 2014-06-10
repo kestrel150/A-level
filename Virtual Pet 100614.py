1a0757f9009a2506f83b70322dbe6f1bf7367dd1
kestrel150 <30732@longroad.ac.uk>
2014-06-10 16:07:08 +0100

    Virtual Pet - Feed system

--- Virtual Pet 100614.py
+++ Virtual Pet 100614.py
@@ -8,13 +8,25 @@ class VirtualPet:
     def __init__(self,name):
         self.name = name
         self.hunger = 100
+        self.food = 5
         print("{0} rises from the ashes!".format(self.name))
 
     #Methods
     def talk(self):
-        print("Hi, im your virtual pet!")
+        if self.hunger >= 5:
+            print("Hi, im your virtual pet!")
+            self.hunger = self.hunger - 5
+        else:
+            print("{0} cannot speak, it is too hungry. (hunger = {1})".format(self.name,self.hunger))
+        
     def hunger_status(self):
         print("Hunger is {0}.".format(self.hunger))
+        feed = input("Would you like to feed {0}[Y/N]? (amount of food = {1}) ".format(self.name,self.food))
+        if feed = "Y":
+            self.hunger = 100
+            self.food = self.food - 1
+        else:
+            print("You chose not to feed your pet")
 
 def main():
     name = input("Please enter your new pet's name: ")
