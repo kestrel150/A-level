44bfc9e04e89cb52dca6d65af583146cb8e96e31
kestrel150 <30732@longroad.ac.uk>
2014-06-10 15:43:46 +0100

    Virtual Pet - Constructor method
    Created constructor method, which applies the pet's name to the instance

--- Virtual Pet 100614.py
+++ Virtual Pet 100614.py
@@ -4,14 +4,19 @@
 
 class VirtualPet:
     """A representation of a pet."""
+    #Constructor - run automatically
+    def __init__(self,name):
+        self.name = PetName
+        print("{0} rises from the ashes!".format(self.name))
 
     #Methods
     def talk(self):
         print("Hi, im your virtual pet!")
 
 def main():
+    PetName = input("Please enter your new pet's name: ")
     #Creates an instance of the class
-    pet_one = VirtualPet()
+    pet_one = VirtualPet(name)
     #Call talk method
     pet_one.talk()
 
