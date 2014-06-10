af896dd52aa6691d95a982e01f1a56624ebb0ef5
kestrel150 <30732@longroad.ac.uk>
2014-06-10 15:55:37 +0100

    Virtual Pet - Hunger
    simple hunger method

--- Virtual Pet 100614.py
+++ Virtual Pet 100614.py
@@ -6,17 +6,28 @@ class VirtualPet:
     """A representation of a pet."""
     #Constructor - run automatically
     def __init__(self,name):
-        self.name = PetName
+        self.name = name
+        self.hunger = 100
         print("{0} rises from the ashes!".format(self.name))
 
     #Methods
     def talk(self):
         print("Hi, im your virtual pet!")
+    def hunger_status(self):
+        print("Hunger is {0}.".format(self.hunger))
 
 def main():
-    PetName = input("Please enter your new pet's name: ")
+    name = input("Please enter your new pet's name: ")
     #Creates an instance of the class
     pet_one = VirtualPet(name)
+    choice = 0
+    while choice != "Q":
+        choice = int(input("Please make your choice: "))
+        if choice == 1:
+            pet_one.talk()
+        if choice == 2:
+            pet_one.hunger_status()
+        
     #Call talk method
     pet_one.talk()
 
