70803e49792fdbaebd66c40b6fb6ddb6e01c0051
kestrel150 <30732@longroad.ac.uk>
2014-06-13 12:16:44 +0100

    Virtual Pet - Level up system Complete
    XP gaining system still required.

--- Virtual Pet/Virtual Pet 100614.py
+++ Virtual Pet/Virtual Pet 100614.py
@@ -23,7 +23,7 @@ class VirtualPet:
         #Basic stats
         self.name = name
         self.hunger = 100
-        self.level = 0
+        self.level = 1
         self.xp = 100
         
         #Combat stats
@@ -31,7 +31,7 @@ class VirtualPet:
         self.mp = 50
         self.pattack = 1
         self.mattack = 1
-        self.speed = 3
+        self.speed = 2
         
         print("{0} rises from the ashes!".format(self.name))
 
@@ -44,14 +44,15 @@ class VirtualPet:
         print("Pet hunger = {0}".format(self.hunger))
         print()
 
-    def level_up_options(self):
-        print("WORK IN PROGRESS")
 
     def level_up(self):
         self.level = self.level + 1
         self.xp = 0
         self.hp = self.hp + 25
         self.mp = self.mp + 10
+        self.pattack = self.pattack + 1
+        self.mattack = self.mattack + 1
+        self.speed = self.speed + 1
         print("**********LEVEL UP**********")
         print()
         print("You have advanced to level {0}!".format(self.level))
@@ -59,9 +60,7 @@ class VirtualPet:
         print("{0}'s MP has increased from {1} to {2}!".format(self.name,self.mp-10,self.mp))
         print("Please choose which stat you wish to level from the list below.")
         print()
-        self.level_up_options()
         
-    
     def talk(self):
         #prints statement
         if self.hunger >= 5:
@@ -70,27 +69,7 @@ class VirtualPet:
         else:
             print("{0} cannot speak, it is too hungry. (hunger = {1})".format(self.name,self.hunger))
         
-    def hunger_status(self,playerstats):
-        #States Hunger value and asks if you wish to feed your pet
-        print("Hunger is {0}.".format(self.hunger))
-        if playerstats.food <= 0:
-            print("You cannot feed {0},as you have no food.".format(self.name))
-        if playerstats.food >0:
-            valid = False
-            while not valid:
-                feed = input("Would you like to feed {0}[Y/N]? (amount of food = {1}) ".format(self.name,playerstats.food))
-                feed = feed[0].upper()
-                if feed == "Y" or feed == "N":
-                    valid = True
-                else:
-                    print("Please enter a valid choice.")
-            if feed == "Y":
-                self.hunger = 100
-                playerstats.food = playerstats.food - 1
-                print("{0}'s health is restored to 100.".format(self.name))
-                print("You have {0} pet food left.".format(playerstats.food))
-            else:
-                print("You chose not to feed your pet.")
+    
         
 
 
@@ -100,6 +79,13 @@ def display_menu():
     print("1.Make your pet speak")
     print("2.Check your pet's hunger status")
     print("0.View stats")
+    print()
+
+def level_up_options(pet_one):
+    print()
+    print("1. Physical Attack: {0} --> {1}".format(pet_one.pattack,pet_one.pattack + 1))
+    print("2. Magical Attack: {0} --> {1}".format(pet_one.mattack,pet_one.mattack + 1))
+    print("3. Speed: {0} --> {1}".format(pet_one.speed,pet_one.speed + 1))
     print()
 
 def pet_name():
@@ -113,7 +99,52 @@ def pet_name():
             print("Please enter a valid name, that is at least 1 letter long.")
             
     return name
-    
+
+
+
+def level_up_pet(pet_one):
+    #Levels up the pet
+    pet_one.level_up()
+    level_up_options(pet_one)
+
+    valid = False
+    while not valid:
+        stat_choice = int(input("Stat Choice: "))
+        if stat_choice in range(1,4):
+            valid = True
+        else:
+            print("Please make a valid stat choice.")
+
+    if stat_choice == 1:
+        pet_one.pattack = pet_one.pattack + 1
+    elif stat_choice == 2:
+        pet_one.mattack = pet_one.mattack + 1
+    elif stat_choice == 3:
+        pet_one.speed = pet_one.speed + 1
+
+
+def hunger_status(playerstats,pet_one):
+        #States Hunger value and asks if you wish to feed your pet
+        print("Hunger is {0}.".format(pet_one.hunger))
+        if playerstats.food <= 0:
+            print("You cannot feed {0},as you have no food.".format(pet_one.name))
+        if playerstats.food >0:
+            valid = False
+            while not valid:
+                feed = input("Would you like to feed {0}[Y/N]? (amount of food = {1}) ".format(pet_one.name,playerstats.food))
+                feed = feed[0].upper()
+                if feed == "Y" or feed == "N":
+                    valid = True
+                else:
+                    print("Please enter a valid choice.")
+            if feed == "Y":
+                pet_one.hunger = 100
+                playerstats.food = playerstats.food - 1
+                print("{0}'s health is restored to 100.".format(pet_one.name))
+                print("You have {0} pet food left.".format(playerstats.food))
+            else:
+                print("You chose not to feed your pet.")
+                
 def main():
     playerstats = Player()
     name = pet_name()
@@ -122,8 +153,9 @@ def main():
     choice = 0
 
     while choice != ("q" or "Q"):
-        if pet_one.xp == 100:
-            pet_one.level_up()
+        xp_threshold = 100*pet_one.level
+        if pet_one.xp >= xp_threshold:
+            level_up_pet(pet_one)
         display_menu()
         print()
         choice = input("Please make your choice (or enter q to quit): ")
@@ -131,7 +163,7 @@ def main():
         if choice == "1":
             pet_one.talk()
         elif choice == "2":
-            pet_one.hunger_status(playerstats)
+            hunger_status(playerstats,pet_one)
         elif choice == "0":
             pet_one.pet_status()
             playerstats.items()
